759a565b1d06dc3b4ed5daef660357ed43eb61c0
Abdullah Essam <abdoessam.2010@gmail.com>
2023-08-13 02:58:31 +0300

    update admin dashboard view

--- resources/views/admins/app.blade.php
+++ resources/views/admins/app.blade.php
@@ -5,8 +5,8 @@
     <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{ env('APP_NAME') }} | {{ __('Dashboard') }}</title>
-    <script type="module" crossorigin src="/assets/admin-dashboard/index-274cdbe7.js"></script>
-    <link rel="stylesheet" href="/assets/admin-dashboard/index-4d0ab883.css">
+    <script type="module" crossorigin src="/assets/admin-dashboard/index-d0b0a32a.js"></script>
+    <link rel="stylesheet" href="/assets/admin-dashboard/index-b32b3e96.css">
   </head>
   <body>
     <div id="app"></div>
