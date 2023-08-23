a688ea53f99a4a405e4ef17fbea4bfcaedc4ed27
Abdullah Essam <abdoessam.2010@gmail.com>
2023-08-23 16:05:57 +0300

    sync local repo with remote

--- resources/views/admins/app.blade.php
+++ resources/views/admins/app.blade.php
@@ -5,8 +5,8 @@
     <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{ env('APP_NAME') }} | {{ __('Dashboard') }}</title>
-    <script type="module" crossorigin src="/assets/admin-dashboard/index-ac2e68a9.js"></script>
-    <link rel="stylesheet" href="/assets/admin-dashboard/index-ce529be9.css">
+    <script type="module" crossorigin src="/assets/admin-dashboard/index-fb62526c.js"></script>
+    <link rel="stylesheet" href="/assets/admin-dashboard/index-6eeff548.css">
   </head>
   <body>
     <div id="app"></div>
