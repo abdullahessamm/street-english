1cb1f5afb61c87150b8795f7c3f0a3ecb4454f80
Abdullah Essam <abdoessam.2010@gmail.com>
2023-10-17 06:31:22 +0300

    update frontend app links

--- resources/views/admins/app.blade.php
+++ resources/views/admins/app.blade.php
@@ -5,8 +5,8 @@
     <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{ env('APP_NAME') }} | {{ __('Dashboard') }}</title>
-    <script type="module" crossorigin src="/assets/admin-dashboard/index-b1350873.js"></script>
-    <link rel="stylesheet" href="/assets/admin-dashboard/index-4693ac1a.css">
+    <script type="module" crossorigin src="/assets/admin-dashboard/index-f941e14e.js"></script>
+    <link rel="stylesheet" href="/assets/admin-dashboard/index-8d397931.css">
   </head>
   <body>
     <div id="app"></div>
