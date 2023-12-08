9b2f1197e87f31b9df6cab80636844c4049c69f1
Abdullah Essam <abdoessam.2010@gmail.com>
2023-12-08 17:34:43 +0200

    zoom course user fix dashboard route

--- routes/zoom.php
+++ routes/zoom.php
@@ -17,10 +17,14 @@ use Illuminate\Support\Facades\Route;
 
 
 
-Route::view('/', 'zoom-users.app')->name('zoomStudent.home')->middleware('auth:web:zoomStudent,zoomStudent.login');
 
 Route::view('/login', 'zoom-users.auth.login')->middleware('guest:web:zoomStudent,zoomStudent.home');
 Route::controller(LoginController::class)->group(function () {
     Route::post('/login', 'login')->name('zoomStudent.login');
     Route::post('/logout', 'logout')->name('zoomStudent.logout');
 });
+
+Route::view('/{params?}', 'zoom-users.app')
+    ->where('param', '.*')
+    ->name('zoomStudent.home')
+    ->middleware('auth:web:zoomStudent,zoomStudent.login');
