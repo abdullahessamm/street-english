50d666c1ce67f3da09d2afb91f66101e49c6acb9
Abdullah Essam <abdoessam.2010@gmail.com>
2023-12-13 00:13:14 +0200

    Update admin dashboard front-end assets url

--- resources/views/admins/app.blade.php
+++ resources/views/admins/app.blade.php
@@ -5,8 +5,8 @@
     <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{ env('APP_NAME') }} | {{ __('Dashboard') }}</title>
-    <script type="module" crossorigin src="/assets/admin-dashboard/index-6282ffd9.js"></script>
-    <link rel="stylesheet" href="/assets/admin-dashboard/index-4bbbee22.css">
+    <script type="module" crossorigin src="/assets/admin-dashboard/index-edcf668c.js"></script>
+    <link rel="stylesheet" href="/assets/admin-dashboard/index-bb473e2e.css">
   </head>
   <body>
     <div id="app"></div>
