0e9407b5f4781d3547782497ba1ba772b53e1d16
Abdullah Essam <abdoessam.2010@gmail.com>
2023-12-13 00:25:33 +0200

    Update instructor dashboard front-end assets url

--- resources/views/instructor/app.blade.php
+++ resources/views/instructor/app.blade.php
@@ -5,8 +5,8 @@
     <link rel="shortcut icon" href="logo.ico" type="image/x-icon">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title> {{ env('APP_NAME') }} | {{ __('Instructor') }}</title>
-    <script type="module" crossorigin src="/assets/instructor-dashboard/index-955e31b5.js"></script>
-    <link rel="stylesheet" href="/assets/instructor-dashboard/index-850f9408.css">
+    <script type="module" crossorigin src="/assets/instructor-dashboard/index-9e27883b.js"></script>
+    <link rel="stylesheet" href="/assets/instructor-dashboard/index-ff1dee1a.css">
   </head>
   <body>
     <div id="app"></div>
