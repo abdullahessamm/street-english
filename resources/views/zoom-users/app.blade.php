7c3003897f33d09ed762cca0b6da10189eea7112
Abdullah Essam <abdoessam.2010@gmail.com>
2023-12-13 20:44:08 +0200

    Update zoom user dashboard front-end assets url

--- resources/views/zoom-users/app.blade.php
+++ resources/views/zoom-users/app.blade.php
@@ -4,9 +4,9 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title> {{ env('APP_NAME') }} | ZOOM </title>
-    <script type="module" crossorigin src="/assets/zoom-student-dashboard/index-7346b6d2.js"></script>
-    <link rel="stylesheet" href="/assets/zoom-student-dashboard/index-8c4d4d59.css">
+    <title> {{ env('APP_NAME') }} | Student </title>
+    <script type="module" crossorigin src="/assets/zoom-student-dashboard/index-b20b6c93.js"></script>
+    <link rel="stylesheet" href="/assets/zoom-student-dashboard/index-ab240c8b.css">
 </head>
 <body>
     <div id="app"></div>
